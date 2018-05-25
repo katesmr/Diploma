586ea134b5a8fbf9def61f422a04e1d1e20de3fc
katesmr <you@exampe.com>
2018-05-25 22:16:10 +0300

    Create test function for offline.

--- project/sound_store/static/js/test.js
+++ project/sound_store/static/js/test.js
@@ -1007,6 +1007,8 @@ var RecorderView = __webpack_require__(71);
 var commonEventNames = __webpack_require__(3);
 var windowsTransport = __webpack_require__(4);
 
+var Recorder = __webpack_require__(90);
+
 module.exports = TrackView;
 
 function TrackView(controller){
@@ -1087,6 +1089,79 @@ function setTrack(eventName, track){
     this.filterTabSegment.setFilter(track);
 
     this.instrumentView.setTrack(track);
+
+    testSave(track);
+}
+
+function testSave(track){
+    console.log(createDuration([{"time": 0, "duration": 0.8}, {"time": 0.3, "duration": 0.4},
+                                {"time": 0.4, "duration": 0.3}, {"time": 0.6, "duration": 0.2},
+                                {"time": 0.65, "duration": 0.3}]));
+    var events = [];
+    var timeData = [];
+    var tmp, temp, i, duration;
+    for(i = 0; i < track.playObjects.length; ++i){
+        tmp = {};
+        temp = {};
+        tmp.time = (track.playObjects[i].triggerAttackTime/1000);
+        tmp.duration = (track.playObjects[i].triggerReleaseTime/1000);
+        temp.time = tmp.time;
+        temp.duration = tmp.duration;
+        tmp.note = (track.playObjects[i].note);
+        events.push(tmp);
+        timeData.push(temp);
+    }
+    console.log(events);
+    duration = createDuration(timeData);
+    console.log(duration);
+
+    /*Tone.Offline(function(){
+        var part = new Tone.Part(function(time, value){
+            //the notes given as the second element in the array
+            //will be passed in as the second argument
+            track.trackObject.triggerAttackRelease(value.note, value.duration, '+' + value.time);
+            console.log(value.note, value.time, value.duration);
+        }, events);
+        part.start(0);
+        Tone.Transport.start();
+    }, duration).then(function(buffer){
+        //do something with the output buffer
+        console.log(buffer);
+        console.log(buffer._buffer);
+        Tone.Transport.stop();
+
+    });*/
+
+    /*Tone.Offline(function(){
+        var part = new Tone.Part(function(time, note, dur){
+            //the notes given as the second element in the array
+            //will be passed in as the second argument
+            track.trackObject.triggerAttackRelease(note, dur, time);
+            console.log(note, time);
+        }, events);
+        part.start(0);
+        Tone.Transport.start();
+    }, 10).then(function(buffer){
+        //do something with the output buffer
+        console.log(buffer);
+        console.log(buffer._buffer);
+
+    });*/
+}
+
+function createDuration(data){
+    var duration = 0;
+    var tmp;
+    var obj;
+    for(obj in data){
+        if(data[obj].time === 0){
+            duration = data[obj].duration;
+        } else{
+            tmp = data[obj].time + data[obj].duration;
+            duration = Math.max(duration, tmp);
+        }
+    }
+    return duration;
 }
 
 
@@ -5465,6 +5540,572 @@ LeftTom.prototype.setInstrument = function(){
 
 
 
+
+
+/***/ }),
+/* 89 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(global) {
+
+var WORKER_ENABLED = !!(global === global.window && global.URL && global.Blob && global.Worker);
+
+function InlineWorker(func, self) {
+  var _this = this;
+  var functionBody;
+
+  self = self || {};
+
+  if (WORKER_ENABLED) {
+    functionBody = func.toString().trim().match(
+      /^function\s*\w*\s*\([\w\s,]*\)\s*{([\w\W]*?)}$/
+    )[1];
+
+    return new global.Worker(global.URL.createObjectURL(
+      new global.Blob([ functionBody ], { type: "text/javascript" })
+    ));
+  }
+
+  function postMessage(data) {
+    setTimeout(function() {
+      _this.onmessage({ data: data });
+    }, 0);
+  }
+
+  this.self = self;
+  this.self.postMessage = postMessage;
+
+  setTimeout(func.bind(self, self), 0);
+}
+
+InlineWorker.prototype.postMessage = function postMessage(data) {
+  var _this = this;
+
+  setTimeout(function() {
+    _this.self.onmessage({ data: data });
+  }, 0);
+};
+
+module.exports = InlineWorker;
+
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(93)))
+
+/***/ }),
+/* 90 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+module.exports = __webpack_require__(91);
+
+
+/***/ }),
+/* 91 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _microphone = __webpack_require__(92);
+
+var _microphone2 = _interopRequireDefault(_microphone);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var defaultConfig = {
+  nFrequencyBars: 255,
+  onAnalysed: null
+};
+
+var Recorder = function () {
+  function Recorder(audioContext) {
+    var config = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
+    _classCallCheck(this, Recorder);
+
+    this.config = Object.assign({}, defaultConfig, config);
+
+    this.audioContext = audioContext;
+    this.audioInput = null;
+    this.realAudioInput = null;
+    this.inputPoint = null;
+    this.audioRecorder = null;
+    this.rafID = null;
+    this.analyserContext = null;
+    this.recIndex = 0;
+    this.stream = null;
+
+    this.updateAnalysers = this.updateAnalysers.bind(this);
+  }
+
+  _createClass(Recorder, [{
+    key: 'init',
+    value: function init(stream) {
+      var _this = this;
+
+      return new Promise(function (resolve) {
+        _this.inputPoint = _this.audioContext.createGain();
+
+        _this.stream = stream;
+
+        _this.realAudioInput = _this.audioContext.createMediaStreamSource(stream);
+        _this.audioInput = _this.realAudioInput;
+        _this.audioInput.connect(_this.inputPoint);
+
+        _this.analyserNode = _this.audioContext.createAnalyser();
+        _this.analyserNode.fftSize = 2048;
+        _this.inputPoint.connect(_this.analyserNode);
+
+        _this.audioRecorder = new _microphone2.default(_this.inputPoint);
+
+        var zeroGain = _this.audioContext.createGain();
+        zeroGain.gain.value = 0.0;
+
+        _this.inputPoint.connect(zeroGain);
+        zeroGain.connect(_this.audioContext.destination);
+
+        _this.updateAnalysers();
+
+        resolve();
+      });
+    }
+  }, {
+    key: 'start',
+    value: function start() {
+      var _this2 = this;
+
+      return new Promise(function (resolve, reject) {
+        if (!_this2.audioRecorder) {
+          reject('Not currently recording');
+          return;
+        }
+
+        _this2.audioRecorder.clear();
+        _this2.audioRecorder.record();
+
+        resolve(_this2.stream);
+      });
+    }
+  }, {
+    key: 'stop',
+    value: function stop() {
+      var _this3 = this;
+
+      return new Promise(function (resolve) {
+        _this3.audioRecorder.stop();
+
+        _this3.audioRecorder.getBuffer(function (buffer) {
+          _this3.audioRecorder.exportWAV(function (blob) {
+            return resolve({ buffer: buffer, blob: blob });
+          });
+        });
+      });
+    }
+  }, {
+    key: 'updateAnalysers',
+    value: function updateAnalysers() {
+      if (this.config.onAnalysed) {
+        requestAnimationFrame(this.updateAnalysers);
+
+        var freqByteData = new Uint8Array(this.analyserNode.frequencyBinCount);
+
+        this.analyserNode.getByteFrequencyData(freqByteData);
+
+        var data = new Array(255);
+        var lastNonZero = 0;
+        var datum = void 0;
+
+        for (var idx = 0; idx < 255; idx += 1) {
+          datum = Math.floor(freqByteData[idx]) - Math.floor(freqByteData[idx]) % 5;
+
+          if (datum !== 0) {
+            lastNonZero = idx;
+          }
+
+          data[idx] = datum;
+        }
+
+        this.config.onAnalysed({ data: data, lineTo: lastNonZero });
+      }
+    }
+  }, {
+    key: 'setOnAnalysed',
+    value: function setOnAnalysed(handler) {
+      this.config.onAnalysed = handler;
+    }
+  }]);
+
+  return Recorder;
+}();
+
+Recorder.download = function download(blob) {
+  var filename = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'audio';
+
+  _microphone2.default.forceDownload(blob, filename + '.wav');
+};
+
+exports.default = Recorder;
+
+/***/ }),
+/* 92 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }(); /* eslint-disable */
+/**
+ * License (MIT)
+ *
+ * Copyright © 2013 Matt Diamond
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+
+var _inlineWorker = __webpack_require__(89);
+
+var _inlineWorker2 = _interopRequireDefault(_inlineWorker);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var defaultConfig = {
+  bufferLen: 4096,
+  numChannels: 2,
+  mimeType: 'audio/wav'
+};
+
+var Microphone = function () {
+  function Microphone(source, config) {
+    var _this = this;
+
+    _classCallCheck(this, Microphone);
+
+    this.config = Object.assign({}, defaultConfig, config);
+
+    this.recording = false;
+
+    this.callbacks = {
+      getBuffer: [],
+      exportWAV: []
+    };
+
+    this.context = source.context;
+    this.node = (this.context.createScriptProcessor || this.context.createJavaScriptNode).call(this.context, this.config.bufferLen, this.config.numChannels, this.config.numChannels);
+
+    this.node.onaudioprocess = function (e) {
+      if (!_this.recording) return;
+
+      var buffer = [];
+      for (var channel = 0; channel < _this.config.numChannels; channel++) {
+        buffer.push(e.inputBuffer.getChannelData(channel));
+      }
+      _this.worker.postMessage({
+        command: 'record',
+        buffer: buffer
+      });
+    };
+
+    source.connect(this.node);
+    this.node.connect(this.context.destination); //this should not be necessary
+
+    var self = {};
+    this.worker = new _inlineWorker2.default(function () {
+      var recLength = 0,
+          recBuffers = [],
+          sampleRate = void 0,
+          numChannels = void 0;
+
+      this.onmessage = function (e) {
+        switch (e.data.command) {
+          case 'init':
+            init(e.data.config);
+            break;
+          case 'record':
+            record(e.data.buffer);
+            break;
+          case 'exportWAV':
+            exportWAV(e.data.type);
+            break;
+          case 'getBuffer':
+            getBuffer();
+            break;
+          case 'clear':
+            clear();
+            break;
+        }
+      };
+
+      function init(config) {
+        sampleRate = config.sampleRate;
+        numChannels = config.numChannels;
+        initBuffers();
+      }
+
+      function record(inputBuffer) {
+        for (var channel = 0; channel < numChannels; channel++) {
+          recBuffers[channel].push(inputBuffer[channel]);
+        }
+        recLength += inputBuffer[0].length;
+      }
+
+      function exportWAV(type) {
+        var buffers = [];
+        for (var channel = 0; channel < numChannels; channel++) {
+          buffers.push(mergeBuffers(recBuffers[channel], recLength));
+        }
+        var interleaved = void 0;
+        if (numChannels === 2) {
+          interleaved = interleave(buffers[0], buffers[1]);
+        } else {
+          interleaved = buffers[0];
+        }
+        var dataview = encodeWAV(interleaved);
+        var audioBlob = new Blob([dataview], { type: type });
+
+        this.postMessage({ command: 'exportWAV', data: audioBlob });
+      }
+
+      function getBuffer() {
+        var buffers = [];
+        for (var channel = 0; channel < numChannels; channel++) {
+          buffers.push(mergeBuffers(recBuffers[channel], recLength));
+        }
+        this.postMessage({ command: 'getBuffer', data: buffers });
+      }
+
+      function clear() {
+        recLength = 0;
+        recBuffers = [];
+        initBuffers();
+      }
+
+      function initBuffers() {
+        for (var channel = 0; channel < numChannels; channel++) {
+          recBuffers[channel] = [];
+        }
+      }
+
+      function mergeBuffers(recBuffers, recLength) {
+        var result = new Float32Array(recLength);
+        var offset = 0;
+        for (var i = 0; i < recBuffers.length; i++) {
+          result.set(recBuffers[i], offset);
+          offset += recBuffers[i].length;
+        }
+        return result;
+      }
+
+      function interleave(inputL, inputR) {
+        var length = inputL.length + inputR.length;
+        var result = new Float32Array(length);
+
+        var index = 0,
+            inputIndex = 0;
+
+        while (index < length) {
+          result[index++] = inputL[inputIndex];
+          result[index++] = inputR[inputIndex];
+          inputIndex++;
+        }
+        return result;
+      }
+
+      function floatTo16BitPCM(output, offset, input) {
+        for (var i = 0; i < input.length; i++, offset += 2) {
+          var s = Math.max(-1, Math.min(1, input[i]));
+          output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
+        }
+      }
+
+      function writeString(view, offset, string) {
+        for (var i = 0; i < string.length; i += 1) {
+          view.setUint8(offset + i, string.charCodeAt(i));
+        }
+      }
+
+      function encodeWAV(samples) {
+        var buffer = new ArrayBuffer(44 + samples.length * 2);
+        var view = new DataView(buffer);
+
+        /* RIFF identifier */
+        writeString(view, 0, 'RIFF');
+        /* RIFF chunk length */
+        view.setUint32(4, 36 + samples.length * 2, true);
+        /* RIFF type */
+        writeString(view, 8, 'WAVE');
+        /* format chunk identifier */
+        writeString(view, 12, 'fmt ');
+        /* format chunk length */
+        view.setUint32(16, 16, true);
+        /* sample format (raw) */
+        view.setUint16(20, 1, true);
+        /* channel count */
+        view.setUint16(22, numChannels, true);
+        /* sample rate */
+        view.setUint32(24, sampleRate, true);
+        /* byte rate (sample rate * block align) */
+        view.setUint32(28, sampleRate * 4, true);
+        /* block align (channel count * bytes per sample) */
+        view.setUint16(32, numChannels * 2, true);
+        /* bits per sample */
+        view.setUint16(34, 16, true);
+        /* data chunk identifier */
+        writeString(view, 36, 'data');
+        /* data chunk length */
+        view.setUint32(40, samples.length * 2, true);
+
+        floatTo16BitPCM(view, 44, samples);
+
+        return view;
+      }
+    }, self);
+
+    this.worker.postMessage({
+      command: 'init',
+      config: {
+        sampleRate: this.context.sampleRate,
+        numChannels: this.config.numChannels
+      }
+    });
+
+    this.worker.onmessage = function (e) {
+      var cb = _this.callbacks[e.data.command].pop();
+      if (typeof cb === 'function') {
+        cb(e.data.data);
+      }
+    };
+  }
+
+  _createClass(Microphone, [{
+    key: 'record',
+    value: function record() {
+      this.recording = true;
+    }
+  }, {
+    key: 'stop',
+    value: function stop() {
+      this.recording = false;
+    }
+  }, {
+    key: 'clear',
+    value: function clear() {
+      this.worker.postMessage({ command: 'clear' });
+    }
+  }, {
+    key: 'getBuffer',
+    value: function getBuffer(cb) {
+      cb = cb || this.config.callback;
+
+      if (!cb) throw new Error('Callback not set');
+
+      this.callbacks.getBuffer.push(cb);
+
+      this.worker.postMessage({ command: 'getBuffer' });
+    }
+  }, {
+    key: 'exportWAV',
+    value: function exportWAV(cb, mimeType) {
+      mimeType = mimeType || this.config.mimeType;
+      cb = cb || this.config.callback;
+
+      if (!cb) throw new Error('Callback not set');
+
+      this.callbacks.exportWAV.push(cb);
+
+      this.worker.postMessage({
+        command: 'exportWAV',
+        type: mimeType
+      });
+    }
+  }]);
+
+  return Microphone;
+}();
+
+Microphone.forceDownload = function forceDownload(blob, filename) {
+  var a = document.createElement('a');
+
+  a.style = 'display: none';
+  document.body.appendChild(a);
+
+  var url = window.URL.createObjectURL(blob);
+
+  a.href = url;
+  a.download = filename;
+  a.click();
+
+  window.URL.revokeObjectURL(url);
+
+  document.body.removeChild(a);
+};
+
+exports.default = Microphone;
+
+/***/ }),
+/* 93 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var g;
+
+// This works in non-strict mode
+g = (function() {
+	return this;
+})();
+
+try {
+	// This works if eval is allowed (see CSP)
+	g = g || Function("return this")() || (1,eval)("this");
+} catch(e) {
+	// This works if the window reference is available
+	if(typeof window === "object")
+		g = window;
+}
+
+// g can still be undefined, but nothing to do about it...
+// We return undefined, instead of nothing here, so it's
+// easier to handle this case. if(!global) { ...}
+
+module.exports = g;
 
 
 /***/ })
